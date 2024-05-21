cb3a81951f84777f678d5fcff0395aaf9ffcf10a
Tomas Tye <ttye75@gmail.com>
2024-05-21 17:03:23 +0200

    added scuffed logo

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link"
+import Image from 'next/image'
+import Logo from './financetrackerlogo.png'
 
 export default function Header(){
     return(
@@ -6,6 +8,14 @@ export default function Header(){
             <Link href="/"><div className="h-16 w-32 bg-red-500 place-content-center text-center">Home</div></Link>
             <Link href="/calendar"><div className="h-16 w-32 bg-red-400 place-content-center text-center">Transactions</div></Link>
             <Link href="/test2"><div className="h-16 w-32 bg-red-300 place-content-center text-center">Testing Page</div></Link>
+            <p className="flex grow justify-end">
+            <Image
+                src={Logo}
+                width={400}
+                height={35}
+                alt="logo"
+            />
+            </p>
         </div>
     )
 }
