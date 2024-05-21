639c3c834d4aec4a7e310303072e47e7551c4582
Tomas Tye <ttye75@gmail.com>
2024-05-21 16:38:49 +0200

    moved calendar code from test page to calendar page

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,18 +1,98 @@
-import { connectToDatabase } from "../db";
-import { Transaction, MonthExpenseData } from "../types";
+'use client'
+
+import { useState, useEffect } from 'react';
+
+import Header from '../header'
+import { Transaction } from "../types";
 import TransactionComp from "../transactions";
-import Header from "../header"
+import TransactionList from './tranlist';
+import DateSwitcher from './calendar';
+import { DateContext } from './date-context';
 
-export default async function Home() {
-    const db = await connectToDatabase("./transactions.db");
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-    const transactions: Transaction[] = await db.all(
-    "SELECT * FROM transactions"
+export default function Home() {
+  const [date, setDate] = useState<Value>(new Date());
+  const value = { date, setDate };
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+
+  //  useEffect(() => {
+  //   const dateString: String = formatDateObject(date)
+  //   console.log(`date value: ${date}`)
+  //   const results = TransactionList(dateString)
+  //   setTransactions(results)
+  //   console.log(`state of transactions: ${transactions}`)
+  //  }, [date])
+
+  useEffect(() => {
+    (async () => {
+      const dateString: String = formatDateObject(date)
+      const results: Transaction[] = await TransactionList(dateString)
+      setTransactions(results)
+      console.log(`state of transactions: ${transactions}`)
+    })();
+
+    return () => {
+
+    }
+
+  }, [date])
+
+
+
+
+  // const dateString: String | void = formatDateObject(date);
+  // const transactions: Promise<Transaction[]> = TransactionList(dateString)
+
+
+
+
+
+
+
+  return (
+    <div>
+      <Header />
+      <div className="flex flex-row">
+      <DateContext.Provider value={value}>
+        <DateSwitcher />
+      </DateContext.Provider>
+      <p className="grow">
+        <TransactionComp data={transactions} />
+      </p>
+      </div>
+      <p className="border border-red-950 text-center">
+        <span className='bold'>Selected Date:</span>{' '}
+        {date?.toString()}
+      </p>
+    </div>
   );
-    return(
-        <main className="flex flex-col">
-            <Header/>
-            <TransactionComp data={transactions} />
-        </main>
-    )
+}
+
+function formatDateObject(date: Value) {
+
+  if (date) {
+    const year: String = date?.getFullYear().toString();
+    const month: Number = date?.getMonth() + 1;
+    let day: String = date?.getDate().toString();
+
+    if(day.length < 2){
+      day = '0' + day;
+    }
+
+    const realMonth:String = '0' + month.toString();
+
+    
+
+    const output: String = year + '-' + realMonth+ '-' + day
+
+    console.log(`formatDateObject success: ${output}`)
+
+    return output;
+  }
+
+  console.log(`date not supplied. supplying default datestring`)
+  return '2024-5-21'
 }
---
+++ src/app/calendar/tranlist.tsx
@@ -0,0 +1,26 @@
+'use server'
+
+import { Message } from "postcss";
+import { connectToDatabase } from "../db";
+import { Transaction } from "../types";
+
+export default async function TransactionList(date: String) {
+
+  const db = await connectToDatabase("./transactions.db");
+  console.log(`db obj: ${typeof(db)}`)
+  console.log(`translist called with datestring: ${date}`)
+
+  try {
+    const transactions: Transaction[] = await db.all(`
+    SELECT * FROM transactions WHERE date = '${date}';
+    `);
+
+    console.log(`query results: ${transactions}`)
+    return transactions
+  } catch(e) {
+    console.log(`query failed: ${e}`)
+  }
+
+ 
+}
+
